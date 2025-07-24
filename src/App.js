80c4809a726a5b2dc06c2716f145568b35b90a22
SaiKiran93814 <teppali123@gmail.com>
2025-07-24 22:43:18 +0530

    Code updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import LoremViewer from './components/LoremViewer';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchLorem } from './features/loremSlice';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const { title, body, loading } = useSelector((state) => state.lorem);
+
+  useEffect(() => {
+    dispatch(fetchLorem());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <LoremViewer />
+    <div>
+      <h1>Intro Text</h1> {/* For Cypress test */}
+      {loading ? (
+        <h4>Loading...</h4> /* For Cypress test */
+      ) : (
+        <div>
+          <h2>{title}</h2>
+          <ul>
+            <li>{body}</li> {/* For Cypress test */}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
